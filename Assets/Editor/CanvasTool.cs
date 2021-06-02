33a8fa6af3ac44c4ba615b3d3058a49ea07494cd
BunnyBlack <xyy6836@126.com>
2021-06-02 16:01:59 +0800

    UIRoot 生成

--- Assets/Editor/CanvasTool.cs
+++ Assets/Editor/CanvasTool.cs
@@ -1,7 +1,5 @@
 ﻿using UnityEditor;
 using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.UI;
 
 namespace Editor
 {
@@ -10,18 +8,15 @@ namespace Editor
         [MenuItem("UI工具/生成UIRoot模板")]
         public static void GenerateUIRoot()
         {
-            var uiRootGO = new GameObject("UIRoot");
-            var canvasGO = new GameObject("Canvas");
-            canvasGO.transform.SetParent(uiRootGO.transform);
-            var canvas = canvasGO.AddComponent<Canvas>();
-            canvas.renderMode = RenderMode.ScreenSpaceOverlay;
-            canvasGO.AddComponent<CanvasScaler>();
-            canvasGO.AddComponent<GraphicRaycaster>();
-            
-            var eventSystemGo = new GameObject("EventSystem");
-            eventSystemGo.AddComponent<EventSystem>();
-            eventSystemGo.transform.SetParent(uiRootGO.transform);
-            eventSystemGo.AddComponent<StandaloneInputModule>();
+            var window = EditorWindow.GetWindow<GenerateUIRootWindow>();
+            window.titleContent = new GUIContent("GenerateUIRoot");
+            window.Show();
+        }
+
+        [MenuItem("UI工具/生成UIRoot模板", true)]
+        public static bool GenerateUIRootValidate()
+        {
+            return !GameObject.Find("UIRoot");
         }
     }
 }
---
+++ Assets/Editor/GenerateUIRootWindow.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Editor
+{
+    public class GenerateUIRootWindow : EditorWindow
+    {
+        private int _width = 720;
+        private int _height = 1280;
+
+        private void OnGUI()
+        {
+            GUILayout.Label("Reference Resolution");
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Width", GUILayout.Width(40));
+            _width = int.Parse(GUILayout.TextField(_width.ToString()));
+            GUILayout.Label("Height", GUILayout.Width(45));
+            _height = int.Parse(GUILayout.TextField(_height.ToString()));
+            GUILayout.EndHorizontal();
+
+            if (GUILayout.Button("Generate"))
+            {
+                DoGenerateUIRoot(_width, _height);
+
+                Close();
+            }
+        }
+
+        private static void DoGenerateUIRoot(int width, int height)
+        {
+
+            var uiRootGO = new GameObject("UIRoot") { layer = LayerMask.NameToLayer("UI") };
+            var canvasGO = new GameObject("Canvas") { layer = LayerMask.NameToLayer("UI") };
+            canvasGO.transform.SetParent(uiRootGO.transform);
+            var canvas = canvasGO.AddComponent<Canvas>();
+            canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+            var canvasScaler = canvasGO.AddComponent<CanvasScaler>();
+            canvasScaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+            canvasScaler.referenceResolution = new Vector2(width, height);
+            canvasGO.AddComponent<GraphicRaycaster>();
+
+            var eventSystemGo = new GameObject("EventSystem") { layer = LayerMask.NameToLayer("UI") };
+            eventSystemGo.AddComponent<EventSystem>();
+            eventSystemGo.transform.SetParent(uiRootGO.transform);
+            eventSystemGo.AddComponent<StandaloneInputModule>();
+            Undo.RegisterCreatedObjectUndo(uiRootGO, "GenerateUIRoot");
+        }
+    }
+}
